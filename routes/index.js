a39eb719255b54a33c9e85beb3018a25ddf0145f
ChemaCabrero <josem.cabrero@hotmail.com>
2015-07-17 09:47:43 +0200

    Rama de Créditos y Fotografía

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET Preguntas */
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET Autor */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Autor' });
+});
 
 
 module.exports = router;
