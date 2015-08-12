0e2274b80560ab57d94e6920cde9992c61d9a895
ChemaCabrero <josem.cabrero@hotmail.com>
2015-08-12 12:32:50 +0200

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,20 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+  models.Comment.find({
+            where: {
+                id: Number(commentId)
+            }
+        }).then(function(comment) {
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else{next(new Error('No existe commentId=' + commentId))}
+    }
+  ).catch(function(error){next(error)});
+};
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
   res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -15,7 +30,7 @@ exports.create = function(req, res) {  // POST /quizes/:quizId/comments
      var i = 0;
      var errores = new Array();
      for (var prop in errors) errores[i++] = {message: errors[prop]};
-         res.render('comments/new.ejs', {comment: comment, errors: errores});
+         res.render('comments/new.ejs', {comment: comment, quizid: req.params.quizId, errors: errores});
   } else {
          comment    // save: guarda en DB campos pregunta y respuesta de quiz
          .save()
@@ -23,3 +38,12 @@ exports.create = function(req, res) {  // POST /quizes/:quizId/comments
   }
 };
 
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+  req.comment.publicado = true;
+
+  req.comment.save( {fields: ["publicado"]})
+    .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
+    .catch(function(error){next(error)});
+
+};
